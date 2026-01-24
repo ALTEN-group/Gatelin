073239cd275af53e725467eefe5edccb79ac8636
LCluber <ludovic.cluber@gmail.com>
2026-01-24 22:00:07 +0100

    feat: update dependencies in package.json and remove performance measurement middleware from app.js

--- swagger/src/app.js
+++ swagger/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { log } = require("@dwtechs/winstan");
-const { endTimer, startTimer } = require("@dwtechs/winstan-plugin-express-perf");
 const helmet = require("helmet");
 const app = express();
 app.use(helmet());
@@ -19,21 +18,11 @@ const pe = process.env;
 const PORT = pe.PORT;
 const BASE_PATH = pe.BASE_PATH || "";
 
-// Mandatory modules for any service
-// const error = require("error");
-// const health = require("health");
-// const listen = require("@internal/serve");
-
-// app.use(express.json());
-// performance measurement starts for any call to the following routes
-app.use(startTimer);
-app.use(express.static(`${__dirname}/public`));
 // Routes
 app.use(
   BASE_PATH + "/",
   swaggerUi.serveFiles(doc),
   swaggerUi.setup(doc, swaggerOptions),
 );
-// Performance measurement ends
-app.use(endTimer);
+
 app.listen(PORT, () => log.info(`App listening on port ${PORT}`));
