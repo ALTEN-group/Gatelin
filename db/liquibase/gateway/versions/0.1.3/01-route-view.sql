55ebcd46b5feb9160f57a6299e04931ef34f7112
LCluber <ludovic.cluber@gmail.com>
2026-02-05 22:43:12 +0100

    feat(routes): update route view to include API details

--- db/liquibase/gateway/versions/0.1.3/01-route-view.sql
+++ db/liquibase/gateway/versions/0.1.3/01-route-view.sql
@@ -1,16 +1,19 @@
 
 create or replace view routes AS
   WITH history AS (
-    SELECT * FROM get_history('public', 'users')
+    SELECT * FROM get_history('public', 'route')
   )
   SELECT r.id, 
   r."serviceId",
-  r.name as "route",
+  r."apiId",
+  a.name as "apiName",
   s.name as "serviceName",
+  r.action as "route",
   r.description, 
   r.pattern,
   r.methods,
   r.jwt,
+  r.protected,
   h.tstamp AS "updatedAt",
   h."consumerId" AS "updaterId",
   h."consumerName" AS "updaterName",
@@ -21,10 +24,11 @@ create or replace view routes AS
   FROM "route" AS r
   -- LEFT OUTER JOIN "access" as a ON a."routeId" = r.id 
   LEFT OUTER JOIN "service" as s ON r."serviceId" = s.id 
+  LEFT OUTER JOIN "api" as a ON r."apiId" = a.id 
   -- LEFT OUTER JOIN "permission" as p ON p."functionalityId" = r."functionalityId" AND p."operationId" = a."operationId" 
   LEFT JOIN history h ON (h.id, h.operation) = (r.id, 'UPDATE')
   LEFT JOIN history h2 ON (h2.id, h2.operation) = (r.id, 'INSERT')
-  GROUP BY r.id, s.name,
+  GROUP BY r.id, a.name, s.name,
   h.tstamp, h."consumerId", h."consumerName", 
   h2.tstamp, h2."consumerId", h2."consumerName"
   ORDER BY r.id ASC
