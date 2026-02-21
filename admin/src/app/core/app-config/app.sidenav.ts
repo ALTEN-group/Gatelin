e5ce013a6511496afb24ae7f22bb000c94c4ae22
Estelle Picq <estelle.picq@alten.com>
2026-02-21 19:31:02 +0100

    feat(sidenav): protect sidenav items with acls

--- admin/src/app/core/app-config/app.sidenav.ts
+++ admin/src/app/core/app-config/app.sidenav.ts
@@ -7,35 +7,53 @@ export const SIDENAV: MenuItem[] = [
     label: $localize`:@@Admin_RoutesNav:Routes`,
     routerLink: `/${AppPaths.ROUTES}`,
     icon: "pi pi-directions",
+    data: {
+      functionality: "routes",
+    },
   },
   {
     id: "consumers",
     label: $localize`:@@Admin_ConsumersNav:Consumers`,
     routerLink: `/${AppPaths.CONSUMERS}`,
     icon: "pi pi-users",
+    data: {
+      functionality: "consumers",
+    },
   },
   {
     id: "services",
     label: $localize`:@@Admin_ServicesNav:Services`,
     routerLink: `/${AppPaths.SERVICES}`,
     icon: "pi pi-directions",
+    data: {
+      functionality: "services",
+    },
   },
   {
     id: "apis",
     label: $localize`:@@Admin_ApisNav:Resources`,
     routerLink: `/${AppPaths.RESOURCES}`,
     icon: "pi pi-directions",
+    data: {
+      functionality: "resources",
+    },
   },
   {
     id: "cors",
     label: $localize`:@@Admin_CorsNav:Cors`,
     routerLink: `/${AppPaths.CORS}`,
     icon: "pi pi-directions",
+    data: {
+      functionality: "cors",
+    },
   },
   {
     id: "operations",
     label: $localize`:@@Admin_OperationsNav:Operations`,
     routerLink: `/${AppPaths.OPERATIONS}`,
     icon: "pi pi-directions",
+    data: {
+      functionality: "operations",
+    },
   },
 ];
