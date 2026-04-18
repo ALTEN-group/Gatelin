d873ec322637871a0d8562f707a97d43544b3a7d
Estelle Picq <estelle.picq@alten.com>
2026-04-18 10:37:48 +0200

    feat(consumers): dont display archived roles

--- admin/src/app/admin/data-access/consumers/consumer.conf.ts
+++ admin/src/app/admin/data-access/consumers/consumer.conf.ts
@@ -15,56 +15,59 @@ import { Consumer } from "app/admin/data-access/consumers/consumer.model";
 
 export const CONSUMER_COLUMNS: (
   payload: ActivatedRouteSnapshot,
-) => StrictCrudItemOptions<Consumer>[] = ({ data }) => [
-  ID_CONFIG,
-  {
-    key: "userId",
-    label: "User ID",
-    controlType: CONTROL_TYPES.INPUT,
-    type: INPUT_TYPES.TEXT,
-    controlOptions: {
-      validators: [required, minlength(3), maxlength(30)],
+) => StrictCrudItemOptions<Consumer>[] = ({ data }) => {
+  const activeRoles = (data.roles as Role[]).filter((role) => !role.archived);
+  return [
+    ID_CONFIG,
+    {
+      key: "userId",
+      label: "User ID",
+      controlType: CONTROL_TYPES.INPUT,
+      type: INPUT_TYPES.TEXT,
+      controlOptions: {
+        validators: [required, minlength(3), maxlength(30)],
+      },
     },
-  },
-  {
-    key: "nickname",
-    label: "Nom",
-    controlType: CONTROL_TYPES.INPUT,
-    type: INPUT_TYPES.TEXT,
-    controlOptions: {
-      validators: [required, minlength(3), maxlength(30)],
+    {
+      key: "nickname",
+      label: "Nom",
+      controlType: CONTROL_TYPES.INPUT,
+      type: INPUT_TYPES.TEXT,
+      controlOptions: {
+        validators: [required, minlength(3), maxlength(30)],
+      },
     },
-  },
-  {
-    key: "accessToken",
-    label: "Access Token",
-    controlType: CONTROL_TYPES.TEXTAREA,
-    columnOptions: {
-      defaultWidth: "300px",
+    {
+      key: "accessToken",
+      label: "Access Token",
+      controlType: CONTROL_TYPES.TEXTAREA,
+      columnOptions: {
+        defaultWidth: "300px",
+      },
+      controlOptions: {
+        validators: [required, minlength(28), maxlength(8000)],
+      },
     },
-    controlOptions: {
-      validators: [required, minlength(28), maxlength(8000)],
+    {
+      key: "refreshToken",
+      label: "Refresh Token",
+      controlType: CONTROL_TYPES.TEXTAREA,
+      columnOptions: {
+        defaultWidth: "300px",
+      },
+      controlOptions: {
+        validators: [required, minlength(28), maxlength(8000)],
+      },
     },
-  },
-  {
-    key: "refreshToken",
-    label: "Refresh Token",
-    controlType: CONTROL_TYPES.TEXTAREA,
-    columnOptions: {
-      defaultWidth: "300px",
+    {
+      key: "roles",
+      label: "Rôles",
+      controlType: CONTROL_TYPES.MULTISELECT,
+      options: toSelectItems<Role>(activeRoles, "name"),
+      controlOptions: {
+        validators: [required],
+      },
     },
-    controlOptions: {
-      validators: [required, minlength(28), maxlength(8000)],
-    },
-  },
-  {
-    key: "roles",
-    label: "Rôles",
-    controlType: CONTROL_TYPES.MULTISELECT,
-    options: toSelectItems<Role>(data.roles, "name"),
-    controlOptions: {
-      validators: [required],
-    },
-  },
-  ...createArchivedConfig(),
-];
+    ...createArchivedConfig(),
+  ];
+};
