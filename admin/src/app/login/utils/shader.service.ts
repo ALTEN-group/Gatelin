3e0192478b92f853556802aba2aec80874ad7f00
LCluber <ludovic.cluber@gmail.com>
2026-07-27 11:50:43 +0200

    refactor(shader): replace loader with HttpClient for shaders

--- admin/src/app/login/utils/shader.service.ts
+++ admin/src/app/login/utils/shader.service.ts
@@ -1,49 +1,59 @@
-import { Injectable } from "@angular/core";
-import { IAssets, IResponse, Loader } from "@lcluber/orbisjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
 import { environment } from "environments/environment";
+import { firstValueFrom, forkJoin } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class ShaderService {
-  private readonly assetsFolder: string = environment.assets;
-  list: IAssets = {
-    shaders: {
-      folder: "shader",
-      files: [
-        { name: "vertex.glsl" },
-        { name: "fragment.glsl" },
-        { name: "vertex-light.glsl" },
-        { name: "fragment-light.glsl" },
-      ],
-    },
-  };
-  loader: Loader = new Loader(
-    this.list,
-    this.assetsFolder,
-    undefined,
-    undefined,
-  );
+  private readonly http = inject(HttpClient);
+  private readonly folder = `${environment.assets}/shader/`;
+  private vertex = "";
+  private vertexLight = "";
+  private fragment = "";
+  private fragmentLight = "";
 
   public async load(): Promise<boolean> {
-    return this.loader.start().then((response: IResponse) => {
-      return response.success;
-    });
+    return firstValueFrom(
+      forkJoin({
+        vertex: this.http.get(`${this.folder}vertex.glsl`, {
+          responseType: "text",
+        }),
+        vertexLight: this.http.get(`${this.folder}vertex-light.glsl`, {
+          responseType: "text",
+        }),
+        fragment: this.http.get(`${this.folder}fragment.glsl`, {
+          responseType: "text",
+        }),
+        fragmentLight: this.http.get(`${this.folder}fragment-light.glsl`, {
+          responseType: "text",
+        }),
+      }),
+    )
+      .then((response) => {
+        this.vertex = response.vertex;
+        this.vertexLight = response.vertexLight;
+        this.fragment = response.fragment;
+        this.fragmentLight = response.fragmentLight;
+        return true;
+      })
+      .catch(() => false);
   }
 
   public get vertexShader(): string {
-    return this.loader.getContent("vertex.glsl") as string;
+    return this.vertex;
   }
 
   public get vertexLightShader(): string {
-    return this.loader.getContent("vertex-light.glsl") as string;
+    return this.vertexLight;
   }
 
   public get fragmentShader(): string {
-    return this.loader.getContent("fragment.glsl") as string;
+    return this.fragment;
   }
 
   public get fragmentLightShader(): string {
-    return this.loader.getContent("fragment-light.glsl") as string;
+    return this.fragmentLight;
   }
 }
