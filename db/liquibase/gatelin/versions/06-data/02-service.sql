40eec6ab5cbbd7b3e0305ff576894093ac6b92b1
LCluber <ludovic.cluber@gmail.com>
2026-08-21 10:35:21 +0200

    refactor(database): update service pattern and remove unused changeset

--- db/liquibase/gatelin/versions/06-data/02-service.sql
+++ db/liquibase/gatelin/versions/06-data/02-service.sql
@@ -3,6 +3,6 @@
 --
 
 INSERT INTO service (name, pattern, core, "creatorId", "creatorName") VALUES
-  ('gatelin',      'gateway', true,  -1, 'system'),
+  ('gatelin',      'gatelin', true,  -1, 'system'),
   ('ms-user-mock', 'users',   false, -1, 'system')
 ON CONFLICT DO NOTHING;
