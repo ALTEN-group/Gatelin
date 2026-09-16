4237b8e0eb7a8ca5794bd72ea7cfdcb7e4245602
github-action-benchmark <github@users.noreply.github.com>
2026-09-16 14:30:23 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f41031c2ef11300f3d79c0fa39f52b0744f6444d

---
+++ dev/perf/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1789569023373,
+  "repoUrl": "https://github.com/ALTEN-group/Gatelin",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "LCluber",
+            "username": "LCluber",
+            "email": "ludovic.cluber@gmail.com"
+          },
+          "committer": {
+            "name": "LCluber",
+            "username": "LCluber",
+            "email": "ludovic.cluber@gmail.com"
+          },
+          "id": "f41031c2ef11300f3d79c0fa39f52b0744f6444d",
+          "message": "chore: add user configuration for k6 container to ensure proper permissions",
+          "timestamp": "2026-09-16T14:07:09Z",
+          "url": "https://github.com/ALTEN-group/Gatelin/commit/f41031c2ef11300f3d79c0fa39f52b0744f6444d"
+        },
+        "date": 1789569021918,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "health: http_req_duration p95",
+            "value": 3.96,
+            "unit": "ms"
+          },
+          {
+            "name": "health: http_req_failed rate",
+            "value": 0,
+            "unit": "%"
+          },
+          {
+            "name": "login: http_req_duration p95",
+            "value": 219.02,
+            "unit": "ms"
+          },
+          {
+            "name": "login: http_req_failed rate",
+            "value": 0,
+            "unit": "%"
+          },
+          {
+            "name": "resource-crud: http_req_duration p95",
+            "value": 6.79,
+            "unit": "ms"
+          },
+          {
+            "name": "resource-crud: http_req_failed rate",
+            "value": 0,
+            "unit": "%"
+          }
+        ]
+      }
+    ]
+  }
+}
